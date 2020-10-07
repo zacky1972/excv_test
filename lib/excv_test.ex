44e0a3282c2fc84db49720cd73e14ede10cc0f8f
Susumu Yamazaki <zacky1972@gmail.com>
2020-10-07 15:19:38 +0900

    create load_nif and hook of test

--- lib/excv_test.ex
+++ lib/excv_test.ex
@@ -1,2 +1,17 @@
 defmodule ExcvTest do
+  require Logger
+
+  @on_load :load_nif
+
+  def load_nif do
+    nif_file = '#{:code.priv_dir(:excv_test)}/libnif'
+
+    case :erlang.load_nif(nif_file, 0) do
+      :ok -> :ok
+      {:error, {:reload, _}} -> :ok
+      {:error, reason} -> Logger.warn("Failed to load NIF: #{inspect(reason)}")
+    end
+  end
+
+  def test, do: raise "NIF test/0 not implemented"
 end
